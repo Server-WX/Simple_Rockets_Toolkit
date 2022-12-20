e2f31aa7e1cdf94328a57f8b53a6174b03a800d6
Server-WX <lfl3518963655@163.com>
2022-12-20 12:42:25 +0800

    Update Career

---
+++ src/com/infinity/util/GeneralDefense.java
@@ -0,0 +1,101 @@
+package com.infinity.util;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * 修改全局零件抗性工具类
+ * 参数：文件名，血量，抗热系数
+ *
+ * 还原默认值时传入默认
+ *
+ * 不需要使用时传空字符串
+ */
+
+public class GeneralDefense {
+    private String fileName;
+    private String damageNumber;
+    private String antiHeat;
+
+    public GeneralDefense(String fileName, String damageNumber, String antiHeat) throws IOException {
+        this.fileName = fileName;
+        this.damageNumber = damageNumber;
+        this.antiHeat = antiHeat;
+
+        SAXReader saxReader = new SAXReader();
+        Properties props = System.getProperties();
+        String filePath = props.getProperty("user.home") + "/Library/Application Support/com.jundroo.SimpleRockets2/UserData/CraftDesigns/";
+        Document read = null;
+
+        try {
+            read = saxReader.read(filePath + fileName + ".xml");
+        } catch (DocumentException e) {
+            System.out.println("文件不存在");
+        }
+
+        Element rootElement = read.getRootElement();
+        List<Element> parts = rootElement.element("Assembly").element("Parts").elements("Part");
+
+        if (damageNumber.equals("DEFAULT")) {
+            parts.forEach(ret -> {
+                Element config = ret.element("Config");
+                if (config.attribute("maxDamage") != null) { // 恢复默认
+                    config.remove(config.attribute("maxDamage"));
+                }
+            });
+        } else {
+            parts.forEach(ret -> {
+                Element config = ret.element("Config");
+
+                if (!damageNumber.equals("")) {
+                    // 零件血量
+                    config.addAttribute("maxDamage", damageNumber);
+                }
+
+            });
+        }
+
+        if (antiHeat.equals("DEFAULT")) {
+            parts.forEach(ret -> {
+                Element config = ret.element("Config");
+                if (config.attribute("heatShield") != null) { // 还原默认值
+                    config.remove(config.attribute("heatShield"));
+                }
+
+            });
+        } else {
+            parts.forEach(ret -> {
+                Element config = ret.element("Config");
+
+                if (!antiHeat.equals("")) {
+                    // 零件抗热系数
+                    config.addAttribute("heatShield", antiHeat);
+                }
+                if (config.attribute("heatShield") != null) { // 还原默认值
+                    config.remove(config.attribute("heatShield"));
+                }
+                if (config.attribute("heatShieldScale") != null) { // 移除内鬼
+                    config.remove(config.attribute("heatShieldScale"));
+                }
+
+            });
+        }
+
+        org.dom4j.io.OutputFormat outputFormat = OutputFormat.createPrettyPrint();
+        outputFormat.setEncoding(StandardCharsets.UTF_8.name());
+        FileOutputStream fos = new FileOutputStream(filePath + fileName + "-Edit.xml");
+        XMLWriter xmlWriter = new XMLWriter(fos, outputFormat);
+        xmlWriter.write(read);
+        xmlWriter.close();
+    }
+}
---
+++ src/com/infinity/util/GeneralMassScale.java
@@ -0,0 +1,69 @@
+package com.infinity.util;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * 修改全局零件的质量大小
+ * 参数：文件名，质量数字
+ * 还原默认传入“默认”
+ */
+
+public class GeneralMassScale {
+    private String fileName;
+    private String massNumber;
+
+    public GeneralMassScale(String fileName, String massNumber) throws IOException {
+        this.fileName = fileName;
+        this.massNumber = massNumber;
+
+        SAXReader saxReader = new SAXReader();
+        Properties props = System.getProperties();
+        String filePath = props.getProperty("user.home") + "/Library/Application Support/com.jundroo.SimpleRockets2/UserData/CraftDesigns/";
+        Document read = null;
+
+        try {
+            read = saxReader.read(filePath + fileName + ".xml");
+        } catch (DocumentException e) {
+            System.out.println("文件不存在");
+        }
+
+        Element rootElement = read.getRootElement();
+        List<Element> parts = rootElement.element("Assembly").element("Parts").elements("Part");
+
+        if (massNumber.equals("DEFAULT")) {
+            parts.forEach(ret -> {
+                Element config = ret.element("Config");
+                if (config.attribute("massScale") != null) { // 还原默认值
+                    config.remove(config.attribute("massScale"));
+                }
+
+            });
+        } else {
+            parts.forEach(ret -> {
+                Element config = ret.element("Config");
+                // 修改零件质量
+                config.addAttribute("massScale", massNumber);
+            });
+        }
+
+
+        // 创建输出流保存文件至存档目录
+        OutputFormat outputFormat = OutputFormat.createPrettyPrint();
+        outputFormat.setEncoding(StandardCharsets.UTF_8.name());
+        FileOutputStream fos = new FileOutputStream(filePath + fileName + "-Edit.xml");
+        XMLWriter xmlWriter = new XMLWriter(fos, outputFormat);
+        xmlWriter.write(read);
+        xmlWriter.close();
+    }
+}
